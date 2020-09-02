df83836e6c1c3ce64de8edef30a2552c1b8dfd24
Rehan Sajid <rehan.sajid@khaleef.com>
2020-09-02 23:04:40 +0500

    Fixing build

--- src/Components/Hooks/useScroll.tsx
+++ src/Components/Hooks/useScroll.tsx
@@ -1,35 +1,34 @@
 import { useState, useEffect } from 'react';
 
 const useScroll = () => {
-    const [lastScrollTop, setLastScrollTop] = useState(0);
-    const [bodyOffset, setBodyOffset] = useState(
-      document.body.getBoundingClientRect()
-    );
-    const [scrollY, setScrollY] = useState(bodyOffset.top);
-    const [scrollX, setScrollX] = useState(bodyOffset.left);
-    const [scrollDirection, setScrollDirection] = useState();
+    // const [lastScrollTop, setLastScrollTop] = useState(0);
+    // const [bodyOffset, setBodyOffset] = useState(
+    //   document.body.getBoundingClientRect()
+    // );
+    // const [scrollY, setScrollY] = useState(bodyOffset.top);
+    // const [scrollX, setScrollX] = useState(bodyOffset.left);
+    // const [scrollDirection, setScrollDirection] = useState();
   
-    const listener = (e: any) => {
-        debugger;
-      setBodyOffset(document.body.getBoundingClientRect());
-      setScrollY(-bodyOffset.top);
-      setScrollX(bodyOffset.left);
-      setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
-      setLastScrollTop(-bodyOffset.top);
-    };
+    // const listener = (e: any) => {
+    //   setBodyOffset(document.body.getBoundingClientRect());
+    //   setScrollY(-bodyOffset.top);
+    //   setScrollX(bodyOffset.left);
+    //   setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
+    //   setLastScrollTop(-bodyOffset.top);
+    // };
   
-    useEffect(() => {
-      window.addEventListener("scroll", listener);
-      return () => {
-        window.removeEventListener("scroll", listener);
-      };
-    });
+    // useEffect(() => {
+    //   window.addEventListener("scroll", listener);
+    //   return () => {
+    //     window.removeEventListener("scroll", listener);
+    //   };
+    // });
   
-    return {
-      scrollY,
-      scrollX,
-      scrollDirection
-    };
+    // return {
+    //   scrollY,
+    //   scrollX,
+    //   scrollDirection
+    // };
 };
 
 export { useScroll };
